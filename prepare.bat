7a557bb0a850c4f1acccef6da40c35b530fd938e
gael beard <gaelgael5@gmail.com>
2025-01-14 13:57:57 +0100

    Init

---
+++ prepare.bat
@@ -0,0 +1,22 @@
+
+MKDIR torrent\config\sonarr
+MKDIR torrent\data\sonarr\Animations
+MKDIR torrent\data\sonarr\Series
+
+MKDIR torrent\config\radarr
+MKDIR torrent\data\radarr\movies
+
+MKDIR torrent\config\prowlarr
+
+MKDIR torrent/config/jackett
+MKDIR torrent/config/prowlarr
+
+MKDIR torrent/config/qbittorrent
+MKDIR torrent/data/qbittorrent
+
+MKDIR torrent/config/transmission
+MKDIR torrent/data/transmission
+MKDIR torrent/data/transmission
+
+
+pause
