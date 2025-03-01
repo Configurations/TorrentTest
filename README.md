12f5ff2642b8673b05bc83b5dafe4193958e6631
gael beard <gaelgael5@gmail.com>
2025-03-01 11:13:44 +0100

    Update bat prepare

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+
+
 # TorrentTest
+
+
+## Sonarr 
+http://192.168.10.156:8989
+
--- prepare.bat
+++ prepare.bat
@@ -1,26 +1,20 @@
+SET CONFIG=/torrent
+SET SHARE=/mnt/smb
 
-MKDIR torrent\config\sonarr
-MKDIR torrent\data\sonarr\Animations
-MKDIR torrent\data\sonarr\Series
 
-MKDIR torrent\config\radarr
-MKDIR torrent\data\radarr\movies
+#configs
+MKDIR /%config%/jackett
+MKDIR /%config%/sonarr
+MKDIR /%config%/prowlarr
+MKDIR /%config%/qbittorrent
+MKDIR /%config%/transmission
+MKDIR /%config%/radarr
+MKDIR /%config%/xteve
+MKDIR /%config%/overseerr
 
-MKDIR torrent\config\prowlarr
-
-MKDIR torrent/config/jackett
-MKDIR torrent/config/prowlarr
-
-MKDIR torrent/config/qbittorrent
-MKDIR torrent/data/qbittorrent
-
-MKDIR torrent/config/transmission
-MKDIR torrent/data/transmission
-MKDIR torrent/data/transmission
-
-MKDIR torrent/config/radarr
-MKDIR torrent/data/radarr
-MKDIR torrent/config/xteve
-MKDIR torrent/config/overseer
-
-pause
+#datas
+MKDIR /%SHARE%/sonarr:/data
+MKDIR /%SHARE%/qbittorrent:/data
+MKDIR /%SHARE%/transmission_download:/downloads
+MKDIR /%SHARE%/transmission_media}:/watch	  
+MKDIR /%SHARE%/$radarr:/data
